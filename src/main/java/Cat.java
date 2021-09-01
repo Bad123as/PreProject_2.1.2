d90982a12d370a8beee38b50708e6820d17881be
beka_991 <beka_991@bk.ru>
2021-09-02 02:45:46 +0600

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,22 @@
+public class Cat {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
